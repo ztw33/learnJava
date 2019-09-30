439c66c84962c3103d94cc3e9a38cc18cb7e06bd
ztw33 <zhutingwei@bytedance.com>
2019-09-30 14:51:27 +0800

    增加死锁代码

---
+++ src/concurrency/deadlock/Chopstick.java
@@ -0,0 +1,14 @@
+package concurrency.deadlock;
+
+public class Chopstick {
+    private boolean taken = false;
+    public synchronized void take() throws InterruptedException {
+        while (taken)
+            wait();
+        taken = true;
+    }
+    public synchronized void drop() {
+        taken = false;
+        notifyAll();
+    }
+}
